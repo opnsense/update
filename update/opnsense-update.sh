e8fdacbbe9ec8b8f5ce67ece634e6060ffd753ab
Franco Fichtner <franco@opnsense.org>
2017-03-01 20:47:01 +0100

    update: version 17.1.3

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -42,7 +42,7 @@ WORKDIR=${WORKPREFIX}/${$}
 KERNELDIR="/boot/kernel"
 PKG="pkg-static"
 ARCH=$(uname -p)
-VERSION="17.1.2"
+VERSION="17.1.3"
 
 if [ ! -f ${ORIGIN} ]; then
 	echo "Missing origin.conf"
