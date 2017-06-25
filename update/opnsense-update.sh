481e688295b39034b1ce10334207b99cc44047f2
Franco Fichtner <franco@opnsense.org>
2017-06-25 13:50:15 +0200

    update: new kernel with 17.1.9

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -42,7 +42,7 @@ WORKDIR=${WORKPREFIX}/${$}
 KERNELDIR="/boot/kernel"
 PKG="pkg-static"
 ARCH=$(uname -p)
-VERSION="17.1.7"
+VERSION="17.1.9"
 
 if [ ! -f ${ORIGIN} ]; then
 	echo "Missing origin.conf"
