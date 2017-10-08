c2810c4432ef939a546cd01a1f00abbdb93c2609
Franco Fichtner <franco@opnsense.org>
2017-10-08 15:06:10 +0200

    update: only print what matters

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -301,13 +301,15 @@ fi
 
 if [ -z "${DO_FORCE}" ]; then
 	# disable kernel if locked
-	if [ -n "${LOCKED_KERNEL}" -a -z "${DO_UPGRADE}" ]; then
+	if [ -n "${DO_KERNEL}" -a -n "${LOCKED_KERNEL}" -a \
+	    -z "${DO_UPGRADE}" ]; then
 		echo "Kernel locked, skipping."
 		DO_KERNEL=
 	fi
 
 	# disable base if locked
-	if [ -n "${LOCKED_BASE}" -a -z "${DO_UPGRADE}" ]; then
+	if [ -n "${DO_BASE}" -a -n "${LOCKED_BASE}" -a \
+	    -z "${DO_UPGRADE}" ]; then
 		echo "Base locked, skipping."
 		DO_BASE=
 	fi
