fb12195add3690bbc8c9e6498fc7acbec87b51b3
Franco Fichtner <franco@opnsense.org>
2018-05-14 06:55:59 +0200

    update: new kernel due to security advisories

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -46,7 +46,7 @@ DEBUGDIR="/usr/lib/debug"
 KERNELDIR="/boot/kernel"
 PKG="pkg-static"
 ARCH=$(uname -p)
-VERSION="18.1.6"
+VERSION="18.1.8"
 
 if [ ! -f ${ORIGIN} ]; then
 	echo "Missing origin.conf"
