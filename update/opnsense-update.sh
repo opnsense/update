31da9710f3446bdef9c0133d6a44b6cce77b45f3
Franco Fichtner <franco@opnsense.org>
2018-08-11 19:32:08 +0200

    update: prepare for 18.7.1

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -46,7 +46,7 @@ DEBUGDIR="/usr/lib/debug"
 KERNELDIR="/boot/kernel"
 PKG="pkg-static"
 ARCH=$(uname -p)
-VERSION="18.7"
+VERSION="18.7.1"
 
 if [ ! -f ${ORIGIN} ]; then
 	echo "Missing ${ORIGIN}" >&2
