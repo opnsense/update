91a1d27697e5c705fc0488824668580dffb4ddd7
Franco Fichtner <franco@opnsense.org>
2018-09-12 20:36:21 +0200

    update: another OS update

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -46,7 +46,7 @@ DEBUGDIR="/usr/lib/debug"
 KERNELDIR="/boot/kernel"
 PKG="pkg-static"
 ARCH=$(uname -p)
-VERSION="18.7.2"
+VERSION="18.7.3"
 
 if [ ! -f ${ORIGIN} ]; then
 	echo "Missing ${ORIGIN}" >&2
