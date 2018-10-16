61ec30bd46d7642b53869310e39bd455f33d2f61
Franco Fichtner <franco@opnsense.org>
2018-10-16 17:51:50 +0200

    update: new kernel/base for 18.7.6 soon

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -47,7 +47,7 @@ DEBUGDIR="/usr/lib/debug"
 KERNELDIR="/boot/kernel"
 PKG="pkg-static"
 ARCH=$(uname -p)
-VERSION="18.7.4"
+VERSION="18.7.6"
 
 if [ ! -f ${ORIGIN} ]; then
 	echo "Missing ${ORIGIN}" >&2
