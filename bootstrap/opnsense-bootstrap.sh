bb70346f0664bb4bb092c712dcfe7d0aac75b45d
Franco Fichtner <franco@opnsense.org>
2019-02-08 15:58:43 +0100

    bootstrap: small tweak

--- bootstrap/opnsense-bootstrap.sh
+++ bootstrap/opnsense-bootstrap.sh
@@ -136,7 +136,7 @@ fi
 if [ -n "${DO_INSECURE}" ]; then
 	if [ -z "${DO_YES}" ]; then
 		echo
-		echo -n "Insecure bootstrap mode selected, are you sure? [y/N]: "
+		echo -n "Insecure mode selected, are you sure? [y/N]: "
 
 		read YN
 		case ${YN} in
