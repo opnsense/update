2f2761db2268a51d873aa9bac4a268dcfa6895c3
Franco Fichtner <franco@opnsense.org>
2021-02-25 15:00:21 +0100

    update: this is set in stone

--- update/opnsense-update.sh
+++ update/opnsense-update.sh
@@ -44,7 +44,7 @@ KERNELDIR="/boot/kernel"
 TEE="/usr/bin/tee -a"
 PRODUCT="OPNsense"
 PKG="pkg-static"
-RELEASE="21.1.1"
+RELEASE="21.1.3"
 
 PENDINGDIR="${WORKPREFIX}/.sets.pending"
 PIPEFILE="${WORKPREFIX}/.upgrade.pipe"
