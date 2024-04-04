f2a68310adc96dda4b49b928a2fe50ef7a27b974
Franco Fichtner <franco@opnsense.org>
2024-04-04 11:08:55 +0200

    revert: this stopped working, but really odd timing

--- src/revert/opnsense-revert.sh
+++ src/revert/opnsense-revert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2016-2023 Franco Fichtner <franco@opnsense.org>
+# Copyright (c) 2016-2024 Franco Fichtner <franco@opnsense.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -133,7 +133,7 @@ for PACKAGE in ${@}; do
 	if [ -z "${DO_RELEASE}" ]; then
 		${PKG} install -f ${PACKAGE}
 	else
-		${PKG} install -f ${WORKDIR}/${PACKAGE}.pkg
+		${PKG} add -f ${WORKDIR}/${PACKAGE}.pkg
 	fi
 
 	${PKG} set -A ${AUTOMATIC} ${PACKAGE}
