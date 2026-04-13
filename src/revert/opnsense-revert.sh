89f183c1a6d74a1e2742f3cfa545aa77e18a3dee
Greelan <53196309+Greelan@users.noreply.github.com>
2026-04-13 17:29:11 +1000

    Implement -H option

--- src/revert/opnsense-revert.sh
+++ src/revert/opnsense-revert.sh
@@ -72,8 +72,8 @@ done
 
 export ASSUME_ALWAYS_YES=yes
 
-# recover repo defaults in case a previous run was aborted
-opnsense-update -cd
+# restore repos if missing as a result of previous aborted run
+opnsense-update -H || true
 
 MIRROR="$(opnsense-update -M)/MINT/${DO_RELEASE#-r }/latest/Latest"
 COREPKG=$(opnsense-version -n 2> /dev/null || true)
